cad775861de52bee2792f33a43da4f16e4aec6fd
Mohtad Ahmed Saeem <mohtadsaeem13@gmail.com>
2025-07-25 00:56:27 +0600

    add new datatable for ct mark list

--- static/js/libraries/dashboard_datatable.js
+++ static/js/libraries/dashboard_datatable.js
@@ -46,4 +46,13 @@ $(document).ready(function () {
             "<'row mt-2'<'col-sm-5'i><'col-sm-7'p>>",
         buttons: ['csv', 'excel', 'pdf', 'print']
     });
+
+
+    $('#ctMarksTable').DataTable({
+        dom:
+            "<'row mb-3'<'col-sm-4'l><'col-sm-4 text-center'B><'col-sm-4'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row mt-2'<'col-sm-5'i><'col-sm-7'p>>",
+        buttons: ['csv', 'excel', 'pdf', 'print']
+    });
 });
