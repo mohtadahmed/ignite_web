05b666019d0a32381219f8c605e9436b49bdd3ed
Mohtad Ahmed Saeem <mohtadsaeem13@gmail.com>
2025-07-29 16:09:00 +0600

    datatables for assignment and faculty dashboard

--- static/js/libraries/dashboard_datatable.js
+++ static/js/libraries/dashboard_datatable.js
@@ -55,4 +55,14 @@ $(document).ready(function () {
             "<'row mt-2'<'col-sm-5'i><'col-sm-7'p>>",
         buttons: ['csv', 'excel', 'pdf', 'print']
     });
+
+    $('#assignment-table').DataTable({
+        dom:
+            "<'row mb-3'<'col-sm-4'l><'col-sm-4 text-center'B><'col-sm-4'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row mt-2'<'col-sm-5'i><'col-sm-7'p>>",
+        buttons: ['csv', 'excel', 'pdf', 'print']
+    });
+
+    $('#assigned-courses-table').DataTable();
 });
