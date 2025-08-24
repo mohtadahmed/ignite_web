295a351cd6b30a1b4c6cc1518fa719b1630b173c
Mohtad Ahmed Saeem <mohtadsaeem13@gmail.com>
2025-08-24 21:32:31 +0600

    new datatable added

--- static/js/libraries/dashboard_datatable.js
+++ static/js/libraries/dashboard_datatable.js
@@ -65,4 +65,36 @@ $(document).ready(function () {
     });
 
     $('#assigned-courses-table').DataTable();
+
+    $('#attendanceMarksTable').DataTable({
+        dom:
+            "<'row mb-3'<'col-sm-4'l><'col-sm-4 text-center'B><'col-sm-4'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row mt-2'<'col-sm-5'i><'col-sm-7'p>>",
+        buttons: ['csv', 'excel', 'pdf', 'print']
+    });
+
+    $('#studentCTMarks').DataTable({
+        dom:
+            "<'row mb-3'<'col-sm-4'l><'col-sm-4 text-center'B><'col-sm-4'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row mt-2'<'col-sm-5'i><'col-sm-7'p>>",
+        buttons: ['csv', 'excel', 'pdf', 'print']
+    });
+
+    $('#studentAttendanceMarks').DataTable({
+        dom:
+            "<'row mb-3'<'col-sm-4'l><'col-sm-4 text-center'B><'col-sm-4'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row mt-2'<'col-sm-5'i><'col-sm-7'p>>",
+        buttons: ['csv', 'excel', 'pdf', 'print']
+    });
+
+    $('#studentFinalExamMarks').DataTable({
+        dom:
+            "<'row mb-3'<'col-sm-4'l><'col-sm-4 text-center'B><'col-sm-4'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row mt-2'<'col-sm-5'i><'col-sm-7'p>>",
+        buttons: ['csv', 'excel', 'pdf', 'print']
+    });
 });
